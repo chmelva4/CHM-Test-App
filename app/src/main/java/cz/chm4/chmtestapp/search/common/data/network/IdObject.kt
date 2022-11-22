adda10e014316121099bc146aa9ee7d7fe5a15a0
Vasek Chmel <vasekchmel@gmail.com>
2022-11-22 12:46:33 +0100

    Added missing json annotations

--- app/src/main/java/cz/chm4/chmtestapp/search/common/data/network/IdObject.kt
+++ app/src/main/java/cz/chm4/chmtestapp/search/common/data/network/IdObject.kt
@@ -1,5 +1,8 @@
 package cz.chm4.chmtestapp.search.common.data.network
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class IdObject(
     val id: Int,
     val name: String
--- app/src/main/java/cz/chm4/chmtestapp/search/common/data/network/ImageObject.kt
+++ app/src/main/java/cz/chm4/chmtestapp/search/common/data/network/ImageObject.kt
@@ -1,5 +1,8 @@
 package cz.chm4.chmtestapp.search.common.data.network
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ImageObject(
     val path: String,
     val usageId: Int,
